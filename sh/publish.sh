43d7a0350886c0768c1f2bd8216dd64bc7fc2c96
Oliver Benns <oliverbenns@gmail.com>
2017-05-02 08:32:31 +0900

    Remove comments

--- sh/publish.sh
+++ sh/publish.sh
@@ -39,16 +39,5 @@ function publish {
       ;;
     esac
 
-  # What if there is no upstream?
   git push
-
-
-  # TODO
-  # If no changes have been made (git diff)
-  #   Post does not currently have changes, did you mean `post edit`?
-  # exit 1
-
-  # git add "$POST_FILE"
-  # git commit -m "Add entry for ${date} / Edit entry for ${date}"
-  # git push origin master
 }
