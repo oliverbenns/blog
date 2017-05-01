fe69232882ab7f9954aad41512c724962127f90f
Oliver Benns <oliverbenns@gmail.com>
2017-05-01 10:34:41 +0900

    Add git commands

--- sh/publish.sh
+++ sh/publish.sh
@@ -14,7 +14,34 @@ function publish {
     exit 1
   fi
 
-  echo "Publishing."
+  git add "$POST_FILE"
+
+  GIT_STATUS="$(git status --porcelain $POST_FILE)"
+  GIT_STATUS_CODE=${GIT_STATUS:0:1}
+
+  case $GIT_STATUS_CODE in
+    "A")
+      git commit $POST_FILE -m "Publish ${POST_FILE}"
+      ;;
+    "M")
+      git commit $POST_FILE -m "Edit ${POST_FILE}"
+      ;;
+    "")
+      # Handle for if !file is at top of this script already.
+      echo "No changes made to ${DATE} post. Did you mean [ edit ] ?"
+      git reset "${POST_FILE}"
+      exit 1
+      ;;
+    *)
+      echo "Unexpected git status \"${GIT_STATUS_CODE}\""
+      git reset "${POST_FILE}"
+      exit 1
+      ;;
+    esac
+
+  # What if there is no upstream?
+  git push
+
 
   # TODO
   # If no changes have been made (git diff)
